e55a732417aca666c05c141be82c7fdcba380df2
kizitoomoit <kzitoomoit9@gmail.com>
2020-03-18 21:43:30 +0300

    updated the ui

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
